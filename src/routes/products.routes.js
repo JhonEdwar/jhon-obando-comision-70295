ddd833b2a68671716bca9de1df901aa8be7312d9
Jhon Edwar <jhonedwar192@gmail.com>
2025-01-05 17:59:55 -0500

    ajustes

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import productModel from "../models/product.model.js";
+import { passportCall } from "../utils/passportCall";
+import { authorization } from "../middlewares/authorization";
 
 const router = Router()
 
-router.get('/',async(req,res)=>{
+router.get('/',passportCall('jwt'),authorization('user'),async(req,res)=>{
     const products=await productModel.find({})
     res.status(200).json(products)
 })
