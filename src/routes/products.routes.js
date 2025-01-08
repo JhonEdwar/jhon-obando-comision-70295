0f16581c5fce10e9e13986b49d63d36c5ce36b54
Jhon Edwar <jhonedwar192@gmail.com>
2025-01-08 18:52:41 -0500

    cart ajustes 3

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -13,30 +13,35 @@ router.get('/',passportCall('jwt'),authorization('user'),async(req,res)=>{
 })
 
 router.post('/addcart/:id',passportCall('jwt'),authorization('user'),async(req,res)=>{
-    const productId=req.params.id
-    const userId=req.user._id
-    const mailUser=req.user.email
+    try {
+        const productId=req.params.id
+        const userId=req.user._id
+        const mailUser=req.user.email
 
-    const cartSelected=await cartModel.findOne({user:userId})
+        const cartSelected=await cartModel.findOne({user:userId})
 
-    const datosCart={
-        product:{ $push: { productId: productId, quantity:1 } },
-        user: userId
+        const datosCart={
+            product:{ $push: { productId: productId, quantity:1 } },
+            user: userId
+        }
+        
+        if(!cartSelected){
+            await cartModel.create(datosCart)
+            await userModel.updateOne({ email: mailUser }, { $set: { cart: datosCart } });
+        } else {
+
+            await cartModel.updateOne(
+                { _id: cartSelected._id },
+                { $push: { products: { productId, quantity: 1 } } }
+            )
+        }
+        res.status(200).send({message:'product create'})
+        
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
     
-    if(!cartSelected){
-        await cartModel.create(datosCart)
-        await userModel.updateOne({ email: mailUser }, { $set: { cart: datosCart } });
-    } else {
-
-        await cartModel.updateOne(
-            { _id: cartSelected._id },
-            { $push: { products: { productId, quantity: 1 } } }
-          )
-
-    }
-  
-    res.status(200).send({message:'product create'})
+    
 })
 
 export default router
